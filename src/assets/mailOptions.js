7acf7149bdcf508cf98d8d1bb9f17f1ddadbda44
Ayushya <jaiswalayushya@gmail.com>
2024-03-24 18:28:59 +0530

    Added verification successfull mail to emailServices

--- src/assets/mailOptions.js
+++ src/assets/mailOptions.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const mailTemplateName = {
-    USER_VERIFICATION_MAIL: 'userVerificationMail'
+    USER_VERIFICATION_MAIL: 'userVerificationMail',
+    VERIFICATION_SUCCESS_MAIL: 'verificationSuccessfulMail'
 }
 
 const mailSubjectLine = {
-    USER_VERIFICATION_MAIL: 'Welcome to Wallet watcher - Verify Your Email and Activate Your Account'
+    USER_VERIFICATION_MAIL: 'Welcome to Wallet watcher - Verify Your Email and Activate Your Account',
+    VERIFICATION_SUCCESS_MAIL: 'Account Verified - Welcome to Wallet watcher'
 }
 
 export {
