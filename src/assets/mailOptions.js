3f519e596cbc9cfa5dc8a4a0f8e39115c95d43ec
Ayushya <jaiswalayushya@gmail.com>
2024-03-24 21:27:31 +0530

    Added accountReactivateMail template to emailServices

--- src/assets/mailOptions.js
+++ src/assets/mailOptions.js
@@ -2,12 +2,14 @@
 
 const mailTemplateName = {
     USER_VERIFICATION_MAIL: 'userVerificationMail',
-    VERIFICATION_SUCCESS_MAIL: 'verificationSuccessfulMail'
+    VERIFICATION_SUCCESS_MAIL: 'verificationSuccessfulMail',
+    USER_REACTIVATE_MAIL: 'accountReactivateMail'
 }
 
 const mailSubjectLine = {
-    USER_VERIFICATION_MAIL: 'Welcome to Wallet watcher - Verify Your Email and Activate Your Account',
-    VERIFICATION_SUCCESS_MAIL: 'Account Verified - Welcome to Wallet watcher'
+    USER_VERIFICATION_MAIL: 'Welcome to Wallet watcher - Verify Your Email and Activate Your Account.',
+    VERIFICATION_SUCCESS_MAIL: 'Account Verified - Welcome to Wallet watcher.',
+    USER_REACTIVATE_MAIL: 'Welcome Back! Your Account is Reactivated.'
 }
 
 export {
