0fa8d680865f9d01358db7e7e14af5d518de72d8
Ayushya <jaiswalayushya@gmail.com>
2024-03-25 14:21:46 +0530

    Added user updated mail to accountManagementServices

--- src/assets/mailOptions.js
+++ src/assets/mailOptions.js
@@ -3,13 +3,15 @@
 const mailTemplateName = {
     USER_VERIFICATION_MAIL: 'userVerificationMail',
     VERIFICATION_SUCCESS_MAIL: 'verificationSuccessfulMail',
-    USER_REACTIVATE_MAIL: 'accountReactivateMail'
+    USER_REACTIVATE_MAIL: 'accountReactivateMail',
+    USER_UPDATED_MAIL: 'userUpdatedMail'
 }
 
 const mailSubjectLine = {
     USER_VERIFICATION_MAIL: 'Welcome to Wallet watcher - Verify Your Email and Activate Your Account.',
     VERIFICATION_SUCCESS_MAIL: 'Account Verified - Welcome to Wallet watcher.',
-    USER_REACTIVATE_MAIL: 'Welcome Back! Your Account is Reactivated.'
+    USER_REACTIVATE_MAIL: 'Welcome Back! Your Account is Reactivated.',
+    USER_UPDATED_MAIL: 'Account Details Successfully Updated'
 }
 
 export {
