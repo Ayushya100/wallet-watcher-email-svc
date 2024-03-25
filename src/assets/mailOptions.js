8a7edce87f48b9b65e77cf4d122ee5bba3d34b78
Ayushya <jaiswalayushya@gmail.com>
2024-03-25 16:47:40 +0530

    Added password update mail to emailServices

--- src/assets/mailOptions.js
+++ src/assets/mailOptions.js
@@ -4,14 +4,16 @@ const mailTemplateName = {
     USER_VERIFICATION_MAIL: 'userVerificationMail',
     VERIFICATION_SUCCESS_MAIL: 'verificationSuccessfulMail',
     USER_REACTIVATE_MAIL: 'accountReactivateMail',
-    USER_UPDATED_MAIL: 'userUpdatedMail'
+    USER_UPDATED_MAIL: 'userUpdatedMail',
+    USER_PASSWORD_UPDATED_MAIL: 'passwordUpdatedMail'
 }
 
 const mailSubjectLine = {
     USER_VERIFICATION_MAIL: 'Welcome to Wallet watcher - Verify Your Email and Activate Your Account.',
     VERIFICATION_SUCCESS_MAIL: 'Account Verified - Welcome to Wallet watcher.',
     USER_REACTIVATE_MAIL: 'Welcome Back! Your Account is Reactivated.',
-    USER_UPDATED_MAIL: 'Account Details Successfully Updated'
+    USER_UPDATED_MAIL: 'Account Details Successfully Updated',
+    USER_PASSWORD_UPDATED_MAIL: 'Password Updated Successfully'
 }
 
 export {
